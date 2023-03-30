69bd73cf1b85dfa146134f30932973d468bf5e9e
licanianul <valentineremia@yahoo.com>
2023-03-30 15:47:32 +0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+// module.exports = nextConfig
+
+const runtimeCaching = require("next-pwa/cache");
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   experimental: {
+//     appDir: true,
+//   },
+//   compiler: {
+//     removeConsole: process.env.NODE_ENV !== "development",
+//   },
+//   // images: {
+//   //   domains: ['res.cloudinary.com', 'www.androidu.ro'],
+//   // },
+// };
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
@@ -8,22 +31,15 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development",
   },
-  // images: {
-  //   domains: ['res.cloudinary.com', 'www.androidu.ro'],
-  // },
-};
 
-// module.exports = nextConfig
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+    skipWaiting: true,
+    sw: "/sw.js",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+  },
 
-const runtimeCaching = require("next-pwa/cache");
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  sw: '/sw.js',
-  skipWaiting: true,
-  runtimeCaching,
+ 
 });
-
-module.exports = withPWA(nextConfig);
